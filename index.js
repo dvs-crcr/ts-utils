7af168b4e02b5585781f6102cb39f42eda280e70
dvs <dvs@crcr.ru>
2021-01-13 13:47:08 +0500

    add utils to index

---
+++ index.js
@@ -0,0 +1,11 @@
+const EventBus = require('./utils/EventBus');
+const isEqual = require('./utils/isEqual');
+const mergeObjects = require('./utils/mergeObjects');
+const Queue = require('./utils/Queue');
+const shallowEqualObjects = require('./utils/shallowEqualObjects');
+
+module.exports = EventBus;
+module.exports = isEqual;
+module.exports = mergeObjects;
+module.exports = Queue;
+module.exports = shallowEqualObjects;
