e9aad7d3b68986f85daf31d7a6aa2805e2e42a0c
dvs <dvs@crcr.ru>
2021-01-13 14:36:20 +0500

    add object map

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as EventBus } from './EventBus';
+export { default as isEqual } from './isEqual';
+export { default as mergeObjects } from './mergeObjects';
+export { default as Queue } from './Queue';
+export { default as shallowEqualObjects } from './shallowEqualObjects';
